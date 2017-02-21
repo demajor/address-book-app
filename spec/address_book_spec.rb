3d3e2ae6874b9053e78c7cc51fe4442773016ac9
Doug Major <demajor@demajorMBP.local>
2017-02-21 15:40:05 -0800

    assignment 7 reading CSVs: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -113,21 +113,27 @@ RSpec.describe AddressBook do
             book.import_from_csv("entries_2.csv")
             # Check the first entry
             entry_one = book.entries[0]
-            check_entry(entry_one, "Tash", "555-456-7890", "tash@tashsultana.com")
+            # check_entry(entry_one, "Tash", "555-456-7890", "tash@tashsultana.com")
+            check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+
         end
 
         it "imports the 2nd entry" do
             book.import_from_csv("entries_2.csv")
             # Check the second entry
             entry_two = book.entries[1]
-            check_entry(entry_two, "Danny", "555-555-4646", "elfman@dannyelfman.com")
+            # check_entry(entry_two, "Danny", "555-555-4646", "elfman@dannyelfman.com")
+            check_entry(entry_two, "Sally", "555-555-4646", "sally@blocmail.com")
+
         end
 
         it "imports the 3rd entry" do
             book.import_from_csv("entries_2.csv")
             # Check the third entry
             entry_three = book.entries[2]
-            check_entry(entry_three, "Chris", "555-555-2037", "chris@crb.com")
+            # check_entry(entry_three, "Chris", "555-555-2037", "chris@blocmail.com")
+            check_entry(entry_three, "Sussie", "555-555-2036", "sussie@blocmail.com")
+
         end
     end
 end
