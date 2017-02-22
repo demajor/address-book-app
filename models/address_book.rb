eb68a5e069ca6c7d810dbb43b98af8811bbf0ba5
Doug Major <demajor@demajorMBP.local>
2017-02-21 16:56:03 -0800

    assignment 8 searching: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -61,4 +61,22 @@ class AddressBook
 
         return nil                
     end
+
+    def iterative_search(name)
+        @entries.each do |entry|
+            if entry.name == name
+                return entry
+            end
+        end
+
+        return nil
+    end
+              
 end
+
+
+
+
+
+
+
